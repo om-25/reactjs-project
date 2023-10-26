9d7d4a767f26a0053c0daa72f6ed0c3e85a98bcd
om-25 <omkarkhilare2525@gmail.com>
2023-10-26 19:56:05 +0530

    added

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <a>--OM NaMaHa ShiVaY--</a>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          // className="App-link"
+          // href="https://reactjs.org"
+          // target="_blank"
+          // rel="noopener noreferrer"
         >
-          Learn React
+         ...BABA MAHAKAL...
         </a>
       </header>
     </div>
